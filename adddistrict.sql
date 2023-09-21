9e07b2350a7acfb4960862aebe6e650e5595d470
Khang0202 <nguyenmanhkhang2002@gmail.com>
2023-09-21 14:05:45 +0700

    database

---
+++ adddistrict.sql
@@ -0,0 +1,762 @@
+﻿INSERT INTO District(idprovince ,district)
+values (1 , N'An Phú'),
+	(1, N'Châu Thành'),
+    (1, N'Châu Phú'),
+    (1, N'Chợ Mới'),
+    (1, N'Long Xuyên'),
+    (1, N'Phú Tân'),
+    (1, N'Tân Châu'),
+    (1, N'Thoại Sơn'),
+    (1, N'Tri Tôn');
+INSERT INTO District (idprovince, district)
+VALUES
+    (2, N'Bà Rịa'),
+    (2, N'Châu Đức'),
+    (2, N'Côn Đảo'),
+    (2, N'Đất Đỏ'),
+    (2, N'Long Điền'),
+    (2, N'Tân Thành'),
+    (2, N'Vũng Tàu');
+INSERT INTO District (idprovince, district)
+VALUES
+	(3, N'Bắc Giang'),
+	(3, N'Hiệp Hòa'),
+	(3, N'Lạng Giang'),
+	(3, N'Lục Nam'),
+	(3, N'Lục Ngạn'),
+	(3, N'Sơn Động'),
+	(3, N'Tân Yên'),
+	(3, N'Việt Yên'),
+	(3, N'Yên Dũng'),
+	(3, N'Yên Thế');
+INSERT INTO District (idprovince, district)
+VALUES
+    (4, N'Ba Bể'),
+    (4, N'Bạch Thông'),
+    (4, N'Chợ Đồn'),
+    (4, N'Chợ Mới'),
+    (4, N'Na Rì'),
+    (4, N'Ngân Sơn'),
+    (4, N'Pác Nặm');
+INSERT INTO District (idprovince, district)
+VALUES
+    (5, N'Đông Hải'),
+    (5, N'Giá Rai'),
+    (5, N'Hòa Bình'),
+    (5, N'Hồng Dân'),
+    (5, N'Phước Long'),
+    (5, N'U Minh'),
+    (5, N'Vĩnh Lợi');
+INSERT INTO District (idprovince, district)
+VALUES
+    (6, N'Gia Bình'),
+    (6, N'Lương Tài'),
+    (6, N'Quế Võ'),
+    (6, N'Tiên Du'),
+    (6, N'Yên Phong'),
+    (6, N'Thành phố Bắc Ninh');
+INSERT INTO District (idprovince, district)
+VALUES
+    (7, N'Ba Tri'),
+    (7, N'Bình Đại'),
+    (7, N'Châu Thành'),
+    (7, N'Chợ Lách'),
+    (7, N'Giồng Trôm'),
+    (7, N'Mỏ Cày Bắc'),
+    (7, N'Mỏ Cày Nam'),
+    (7, N'Thạnh Phú'),
+    (7, N'Thành phố Bến Tre');
+INSERT INTO District (idprovince, district)
+VALUES
+    (8, N'An Lão'),
+    (8, N'An Nhơn'),
+    (8, N'Hoài Ân'),
+    (8, N'Hoài Nhơn'),
+    (8, N'Phù Cát'),
+    (8, N'Phù Mỹ'),
+    (8, N'Quy Nhơn'),
+    (8, N'Tây Sơn'),
+    (8, N'Tây Sơn'),
+    (8, N'Vân Canh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (9, N'Bàu Bàng'),
+    (9, N'Bến Cát'),
+    (9, N'Dầu Tiếng'),
+    (9, N'Dĩ An'),
+    (9, N'Phú Giáo'),
+    (9, N'Thủ Dầu Một'),
+    (9, N'Tân Uyên'),
+    (9, N'Uông Bí');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (10, N'Bình Long'),
+    (10, N'Bù Đăng'),
+    (10, N'Bù Đốp'),
+    (10, N'Bù Gia Mập'),
+    (10, N'Chơn Thành'),
+    (10, N'Dong Xoai'),
+    (10, N'Hớn Quản'),
+    (10, N'Lộc Ninh'),
+    (10, N'Phú Riềng');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (11, N'Bắc Bình'),
+    (11, N'Hàm Thuận Bắc'),
+    (11, N'Hàm Thuận Nam'),
+    (11, N'Phan Thiết'),
+    (11, N'Tánh Linh'),
+    (11, N'Tuy Phong');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (12, N'Cà Mau'),
+    (12, N'Cái Nước'),
+    (12, N'Dam Doi'),
+    (12, N'Năm Căn'),
+    (12, N'Ngọc Hiển'),
+    (12, N'Phú Tân'),
+    (12, N'Thới Bình'),
+    (12, N'Trần Văn Thời'),
+    (12, N'U Minh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (13, N'Bình Thủy'),
+    (13, N'Cái Răng'),
+    (13, N'Cờ Đỏ'),
+    (13, N'Ninh Kiều'),
+    (13, N'Ô Môn'),
+    (13, N'Thốt Nốt');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (14, N'Bảo Lâm'),
+    (14, N'Bảo Lạc'),
+    (14, N'Hạ Lang'),
+    (14, N'Hà Quảng'),
+    (14, N'Hoà An'),
+    (14, N'Nguyên Bình'),
+    (14, N'Phục Hòa'),
+    (14, N'Quảng Uyên'),
+    (14, N'Thạch An'),
+    (14, N'Thông Nông'),
+    (14, N'Trà Lĩnh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (15, N'Cẩm Lệ'),
+    (15, N'Hải Châu'),
+    (15, N'Hòa Vang'),
+    (15, N'Liên Chiểu'),
+    (15, N'Ngũ Hành Sơn'),
+    (15, N'Sơn Trà'),
+    (15, N'Thanh Khê');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (16, N'Buôn Đôn'),
+    (16, N'Buôn Hồ'),
+    (16, N'Buôn Ma Thuột (Thành phố)'),
+    (16, N'Cư Kuin'),
+    (16, N'Ea Hleo'),--thêm nháy đơn Ea H'leo
+    (16, N'Ea Kar'),
+    (16, N'Ea Súp'),
+    (16, N'Krông Ana'),
+    (16, N'Krông Búk'),
+    (16, N'Krông Năng');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (17, N'Cư Jút'),
+    (17, N'Đăk Glong'),
+    (17, N'Đắk Mil'),
+    (17, N'Đắk RLấp'),--thêm nháy đơn Đắk R'Lấp
+    (17, N'Krông Nô'),
+    (17, N'Tuy Đức');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (18, N'Diên Biên'),
+    (18, N'Diên Biên Đông'),
+    (18, N'Diên Biên Phủ'),
+    (18, N'Mường Ảng'),
+    (18, N'Mường Chà'),
+    (18, N'Mường Lay'),
+    (18, N'Mường Nhé'),
+    (18, N'Nậm Pồ');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (19, N'Biên Hòa'),
+    (19, N'Cẩm Mỹ'),
+    (19, N'Định Quán'),
+    (19, N'Long Khánh'),
+    (19, N'Long Thành'),
+    (19, N'Nhơn Trạch'),
+    (19, N'Tân Phú'),
+    (19, N'Thống Nhất'),
+    (19, N'Trảng Bom'),
+    (19, N'Vĩnh Cửu'),
+    (19, N'Xuân Lộc');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (20, N'Cao Lãnh'),
+    (20, N'Châu Thành'),
+    (20, N'Hồng Ngự'),
+    (20, N'Lai Vung'),
+    (20, N'Lấp Vò'),
+    (20, N'Tam Nông'),
+    (20, N'Tân Hồng'),
+    (20, N'Tân Phú'),
+    (20, N'Thanh Bình'),
+    (20, N'Tràm Chim');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (21, N'Ayun Pa'),
+    (21, N'Chư Păh'),
+    (21, N'Chư Prông'),
+    (21, N'Chư Sê'),
+    (21, N'Đăk Đoa'),
+    (21, N'Đăk Pơ'),
+    (21, N'Đức Cơ'),
+    (21, N'Ia Grai'),
+    (21, N'Ia Pa'),
+    (21, N'KBang'),
+    (21, N'Kông Chro'),
+    (21, N'Krông Pa');
+INSERT INTO District (idprovince, district)
+VALUES
+    (22, N'Bắc Mê'),
+    (22, N'Bắc Quang'),
+    (22, N'Dong Van'),
+    (22, N'Hà Giang'),
+    (22, N'Hoàng Su Phì'),
+    (22, N'Quản Bạ'),
+    (22, N'Quang Bình'),
+    (22, N'Vị Xuyên'),
+    (22, N'Xín Mần'),
+    (22, N'Yên Minh');
+INSERT INTO District (idprovince, district)
+VALUES
+    (23, N'Bình Lục'),
+    (23, N'Duy Tiên'),
+    (23, N'Kim Bảng'),
+    (23, N'Lý Nhân'),
+    (23, N'Phủ Lý'),
+    (23, N'Thanh Liêm');
+INSERT INTO District (idprovince, district)
+VALUES
+    (24, N'Ba Đình'),
+    (24, N'Cầu Giấy'),
+    (24, N'Đống Đa'),
+    (24, N'Hà Đông'),
+    (24, N'Hoàn Kiếm'),
+    (24, N'Hoàng Mai'),
+    (24, N'Long Biên'),
+    (24, N'Nam Từ Liêm'),
+    (24, N'Tây Hồ'),
+    (24, N'Thanh Trì'),
+    (24, N'Gia Lâm'),
+    (24, N'Đan Phượng'),
+    (24, N'Hoài Đức'),
+    (24, N'Mỹ Đức'),
+    (24, N'Phú Xuyên'),
+    (24, N'Phúc Thọ'),
+    (24, N'Quốc Oai'),
+    (24, N'Sơn Tây'),
+    (24, N'Ứng Hòa'),
+    (24, N'Thạch Thất'),
+    (24, N'Thanh Oai'),
+    (24, N'Ba Vì'),
+    (24, N'Mê Linh');
+INSERT INTO District (idprovince, district)
+VALUES
+    (25, N'Cẩm Xuyên'),
+    (25, N'Can Lộc'),
+    (25, N'Hà Tĩnh'),
+    (25, N'Hương Khê'),
+    (25, N'Hương Sơn'),
+    (25, N'Kỳ Anh'),
+    (25, N'Lộc Hà'),
+    (25, N'Thạch Hà'),
+    (25, N'Vũ Quang');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (26, N'Bình Giang'),
+    (26, N'Cẩm Giàng'),
+    (26, N'Chí Linh'),
+    (26, N'Gia Lộc'),
+    (26, N'Hải Dương'),
+    (26, N'Kim Thành'),
+    (26, N'Kinh Môn'),
+    (26, N'Nam Sách'),
+    (26, N'Ninh Giang'),
+    (26, N'Thanh Hà'),
+    (26, N'Thanh Miện'),
+    (26, N'Tứ Kỳ');
+
+	INSERT INTO District (idprovince, district)
+VALUES
+    (27, N'An Dương'),
+    (27, N'An Lão'),
+    (27, N'Biên Giang'),
+    (27, N'Đồ Sơn'),
+    (27, N'Dương Kinh'),
+    (27, N'Hải An'),
+    (27, N'Hồng Bàng'),
+    (27, N'Kiến An'),
+    (27, N'Kiến Thụy'),
+    (27, N'Lê Chân'),
+    (27, N'Ngô Quyền'),
+    (27, N'Thủy Nguyên'),
+    (27, N'Tiên Lãng'),
+    (27, N'Vĩnh Bảo');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (28, N'Châu Thành'),
+    (28, N'Châu Thành A'),
+    (28, N'Châu Thành B'),
+    (28, N'Long Mỹ'),
+    (28, N'Ngã Bảy'),
+    (28, N'Phụng Hiệp'),
+    (28, N'Vị Thanh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (29, N'Cao Phong'),
+    (29, N'Đà Bắc'),
+    (29, N'Hòa Bình'),
+    (29, N'Kim Bôi'),
+    (29, N'Lạc Sơn'),
+    (29, N'Lạc Thủy'),
+    (29, N'Luông Sơn'),
+    (29, N'Kỳ Sơn'),
+    (29, N'Mai Châu'),
+    (29, N'Tân Lạc'),
+    (29, N'Yên Thủy');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (30, N'Ân Thi'),
+    (30, N'Khoái Châu'),
+    (30, N'Mỹ Hào'),
+    (30, N'Phù Cừ'),
+    (30, N'Tiên Lữ'),
+    (30, N'Văn Giang'),
+    (30, N'Văn Lâm'),
+    (30, N'Văn Lễ'),
+    (30, N'Yên Mỹ');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (31, N'Cẩm Lệ'),
+    (31, N'Diên Khánh'),
+    (31, N'Khánh Sơn'),
+    (31, N'Khánh Vĩnh'),
+    (31, N'Nha Trang'),
+    (31, N'Ninh Hòa'),
+    (31, N'Vạn Ninh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (32, N'An Biên'),
+    (32, N'An Minh'),
+    (32, N'Châu Thành'),
+    (32, N'Giang Thành'),
+    (32, N'Hà Tiên'),
+    (32, N'Hòn Đất'),
+    (32, N'Kiên Hải'),
+    (32, N'Kiên Lương'),
+    (32, N'Phú Quốc'),
+    (32, N'Rạch Giá'),
+    (32, N'Tân Hiệp'),
+    (32, N'U Minh Thượng');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (33, N'Dak Glei'),
+    (33, N'Đắk Hà'),
+    (33, N'Đắk Tô'),
+    (33, N'Ia HDrai'),--thêm nháy đơn Ia H'Drai
+    (33, N'Kon Plông'),
+    (33, N'Kon Rẫy'),
+    (33, N'Kon Tum'),
+    (33, N'Ngọc Hồi'),
+    (33, N'Sa Thầy'),
+    (33, N'Tu Mơ Rông');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (34, N'Mường Tè'),
+    (34, N'Phong Thổ'),
+    (34, N'Sìn Hồ'),
+    (34, N'Tam Đường'),
+    (34, N'Tan Uyên'),
+    (34, N'Lai Châu'),
+    (34, N'Mường Ảng'),
+    (34, N'Nậm Nhùn');
+INSERT INTO District (idprovince, district)
+VALUES
+    (35, N'Bảo Lâm'),
+    (35, N'Bảo Lộc'),
+    (35, N'Cát Tiên'),
+    (35, N'Da Huoai'),
+    (35, N'Di Linh'),
+    (35, N'Dức Trọng'),
+    (35, N'Đạ Huoai'),
+    (35, N'Đạ Tẻh'),
+    (35, N'Lâm Hà'),
+    (35, N'Lạc Dương'),
+    (35, N'Lâm Hà'),
+    (35, N'Di Linh');
+INSERT INTO District (idprovince, district)
+VALUES
+    (36, N'Bắc Sơn'),
+    (36, N'Bình Gia'),
+    (36, N'Cao Lộc'),
+    (36, N'Hữu Lũng'),
+    (36, N'Lạng Sơn'),
+    (36, N'Lộc Bình'),
+    (36, N'Thái Bình'),
+    (36, N'Văn Lãng');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (37, N'Bắc Hà'),
+    (37, N'Bảo Thắng'),
+    (37, N'Bảo Yên'),
+    (37, N'Bát Xát'),
+    (37, N'Muộn Thịnh'),
+    (37, N'Sa Pa'),
+    (37, N'Si Ma Cai'),
+    (37, N'Văn Bàn');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (38, N'Ben Lức'),
+    (38, N'Cần Đước'),
+    (38, N'Cần Giuộc'),
+    (38, N'Châu Thành'),
+    (38, N'Đức Hòa'),
+    (38, N'Đức Huệ'),
+    (38, N'Kiến Tường'),
+    (38, N'Mộc Hóa'),
+    (38, N'Tân An'),
+    (38, N'Tân Hưng'),
+    (38, N'Tân Thạnh'),
+    (38, N'Tân Trụ'),
+    (38, N'Trảng Bàng');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (39, N'Giao Thủy'),
+    (39, N'Hải Hậu'),
+    (39, N'Mỹ Lộc'),
+    (39, N'Nam Trực'),
+    (39, N'Nam Định'),
+    (39, N'Nghĩa Hưng'),
+    (39, N'Xuân Trường'),
+    (39, N'Trực Ninh'),
+    (39, N'Vụ Bản');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (40, N'Anh Sơn'),
+    (40, N'Con Cuông'),
+    (40, N'Cửa Lò'),
+    (40, N'Diễn Châu'),
+    (40, N'Đô Lương'),
+    (40, N'Hưng Nguyên'),
+    (40, N'Kỳ Sơn'),
+    (40, N'Nam Đàn'),
+    (40, N'Nghi Lộc'),
+    (40, N'Nghĩa Đàn'),
+    (40, N'Quế Phong'),
+    (40, N'Quỳ Châu'),
+    (40, N'Quỳ Hợp'),
+    (40, N'Quỳnh Lưu'),
+    (40, N'Thái Hòa'),
+    (40, N'Tương Dương'),
+    (40, N'Yên Thành'),
+    (40, N'Vinh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (41, N'Gia Viễn'),
+    (41, N'Hoa Lư'),
+    (41, N'Kim Sơn'),
+    (41, N'Nho Quan'),
+    (41, N'Ninh Bình'),
+    (41, N'Tam Điệp'),
+    (41, N'Yên Khánh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (42, N'Bác Ái'),
+    (42, N'Ninh Hải'),
+    (42, N'Ninh Phước'),
+    (42, N'Ninh Sơn'),
+    (42, N'Phan Rang - Tháp Chàm'),
+    (42, N'Thuận Bắc'),
+    (42, N'Thuận Nam');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (43, N'Cẩm Khê'),
+    (43, N'Đoan Hùng'),
+    (43, N'Hạ Hòa'),
+    (43, N'Lâm Thao'),
+    (43, N'Phú Thọ'),
+    (43, N'Tam Nông'),
+    (43, N'Tân Sơn'),
+    (43, N'Thanh Ba'),
+    (43, N'Yên Lập'),
+    (43, N'Phù Ninh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (44, N'Đông Hòa'),
+    (44, N'Đồng Xuân'),
+    (44, N'Phú Hòa'),
+    (44, N'Sông Cầu'),
+    (44, N'Sông Hinh'),
+    (44, N'Tây Hòa'),
+    (44, N'Tuy An'),
+    (44, N'Tuy Hòa');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (45, N'Bố Trạch'),
+    (45, N'Đồng Hới'),
+    (45, N'Lệ Thủy'),
+    (45, N'Min Đức'),
+    (45, N'Quảng Ninh'),
+    (45, N'Quảng Trạch'),
+    (45, N'Tuyên Hóa');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (46, N'Bắc Trà My'),
+    (46, N'Đại Lộc'),
+    (46, N'Điện Bàn'),
+    (46, N'Duy Xuyên'),
+    (46, N'Hội An'),
+    (46, N'Nam Giang'),
+    (46, N'Nông Sơn'),
+    (46, N'Núi Thành'),
+    (46, N'Phú Ninh'),
+    (46, N'Quế Sơn'),
+    (46, N'Tây Giang'),
+    (46, N'Thăng Bình'),
+    (46, N'Tiên Phước');
+
+
+
+
+
+	INSERT INTO District (idprovince, district)
+VALUES
+    (47, N'Ba Tơ'),
+    (47, N'Bình Sơn'),
+    (47, N'Duc Pho'),
+    (47, N'Lý Sơn'),
+    (47, N'Min Đức'),
+    (47, N'Nghĩa Hành'),
+    (47, N'Quảng Ngãi (Thành phố)'),
+    (47, N'Sơn Hà'),
+    (47, N'Sơn Tây'),
+    (47, N'Tư Nghĩa'),
+    (47, N'Trà Bồng'),
+    (47, N'Trà Bồng');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (48, N'Ba Chẽ'),
+    (48, N'Bình Liêu'),
+    (48, N'Cô Tô'),
+    (48, N'Đầm Hà'),
+    (48, N'Dông Triều'),
+    (48, N'Hải Hà'),
+    (48, N'Hòn Đất'),
+    (48, N'Quảng Yên'),
+    (48, N'Uông Bí'),
+    (48, N'Vân Đồn'),
+    (48, N'Yên Hưng');
+INSERT INTO District (idprovince, district)
+VALUES
+    (49, N'Cam Lộ'),
+    (49, N'Cam Lộ'),
+    (49, N'Con Cưng'),
+    (49, N'Đa Krông'),
+    (49, N'Gio Linh'),
+    (49, N'Hải Lăng'),
+    (49, N'Hướng Hóa'),
+    (49, N'Quảng Trị'),
+    (49, N'Triệu Phong'),
+    (49, N'Vĩnh Linh');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (50, N'Châu Thành'),
+    (50, N'Cu Lao Dung'),
+    (50, N'Kế Sách'),
+    (50, N'Long Phú'),
+    (50, N'Mỹ Tú'),
+    (50, N'Mỹ Xuyên'),
+    (50, N'Ngã Năm'),
+    (50, N'Sóc Trăng'),
+    (50, N'Thạnh Trị'),
+    (50, N'Trần Đề'),
+    (50, N'Vĩnh Châu');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (51, N'Bắc Yên'),
+    (51, N'Mai Sơn'),
+    (51, N'Mộc Châu'),
+    (51, N'Mường La'),
+    (51, N'Phù Yên'),
+    (51, N'Quỳnh Nhai'),
+    (51, N'Sơn La'),
+    (51, N'Sông Mã'),
+    (51, N'Thường Xuân'),
+    (51, N'Yên Châu');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (52, N'Bến Cầu'),
+    (52, N'Châu Thành'),
+    (52, N'Dương Minh Châu'),
+    (52, N'Gò Dầu'),
+    (52, N'Hòa Thành'),
+    (52, N'Tân Biên'),
+    (52, N'Tân Châu'),
+    (52, N'Trảng Bàng');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (53, N'Đông Hưng'),
+    (53, N'Hưng Hà'),
+    (53, N'Kiến Xương'),
+    (53, N'Quỳnh Phụ'),
+    (53, N'Thái Bình'),
+    (53, N'Thái Thụy'),
+    (53, N'Tiền Hải'),
+    (53, N'Vũ Thư');
+
+
+
+
+
+	INSERT INTO District (idprovince, district)
+VALUES
+    (54, N'Dại Từ'),
+    (54, N'Định Hóa'),
+    (54, N'Đồng Hỷ'),
+    (54, N'Phổ Yên'),
+    (54, N'Sông Công'),
+    (54, N'Thái Nguyên'),
+    (54, N'Võ Nhai');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (55, N'Bá Thước'),
+    (55, N'Cẩm Thủy'),
+    (55, N'Đông Sơn'),
+    (55, N'Hà Trung'),
+    (55, N'Hậu Lộc'),
+    (55, N'Hoằng Hóa'),
+    (55, N'Lang Chánh'),
+    (55, N'Ngọc Lặc'),
+    (55, N'Nga Sơn'),
+    (55, N'Như Xuân'),
+    (55, N'Nông Cống'),
+    (55, N'Quan Hóa'),
+    (55, N'Quan Sơn'),
+    (55, N'Quảng Xương'),
+    (55, N'Thạch Thành'),
+    (55, N'Thanh Hóa'),
+    (55, N'Thiệu Hóa'),
+    (55, N'Tĩnh Gia'),
+    (55, N'Vĩnh Lộc'),
+    (55, N'Yên Định');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (56, N'A Lưới'),
+    (56, N'Hương Thủy'),
+    (56, N'Hương Trà'),
+    (56, N'Nam Đông'),
+    (56, N'Phong Điền'),
+    (56, N'Phú Lộc'),
+    (56, N'Phú Vang'),
+    (56, N'Quảng Điền'),
+    (56, N'Thị xã Hương Thủy'),
+	(56, N'Thành phố Huế');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (57, N'Cai Lậy'),
+    (57, N'Cái Bè'),
+    (57, N'Cù Lao Dung'),
+    (57, N'Châu Thành'),
+    (57, N'Chợ Gạo'),
+    (57, N'Gò Công'),
+    (57, N'Gò Công Đông'),
+    (57, N'Gò Công Tây'),
+    (57, N'Mỹ Tho'),
+    (57, N'Tân Phước'),
+    (57, N'Vĩnh Long');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (58, N'Càng Long'),
+    (58, N'Châu Thành'),
+    (58, N'Cầu Kè'),
+    (58, N'Duyên Hải'),
+    (58, N'Tiểu Cần'),
+    (58, N'Trà Cú'),
+    (58, N'Trà Vinh'),
+    (58, N'Long Điền');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (59, N'Chiêm Hóa'),
+    (59, N'Hàm Yên'),
+    (59, N'Na Hang'),
+    (59, N'Sơn Dương'),
+    (59, N'Tuyên Quang'),
+    (59, N'Yên Sơn');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (60, N'Bình Minh'),
+    (60, N'Long Hồ'),
+    (60, N'Mang Thít'),
+    (60, N'Tam Bình'),
+    (60, N'Trà Ôn'),
+    (60, N'Vũng Liêm'),
+    (60, N'Vĩnh Long');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (61, N'Bình Xuyên'),
+    (61, N'Lập Thạch'),
+    (61, N'Phúc Yên'),
+    (61, N'Sông Lô'),
+    (61, N'Tam Đảo'),
+    (61, N'Tam Dương'),
+    (61, N'Vĩnh Tường'),
+    (61, N'Yên Lạc');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (62, N'Lục Yên'),
+    (62, N'Mù Căng Chải'),
+    (62, N'Trạm Tấu'),
+    (62, N'Văn Chấn'),
+    (62, N'Văn Yên'),
+    (62, N'Yên Bái'),
+    (62, N'Yên Bình');
+	INSERT INTO District (idprovince, district)
+VALUES
+    (63, N'Quận 1'),
+    (63, N'Quận 2'),
+    (63, N'Quận 3'),
+    (63, N'Quận 4'),
+    (63, N'Quận 5'),
+    (63, N'Quận 6'),
+    (63, N'Quận 7'),
+    (63, N'Quận 8'),
+    (63, N'Quận 9'),
+    (63, N'Quận 10'),
+    (63, N'Quận 11'),
+    (63, N'Quận 12'),
+    (63, N'Bình Tân'),
+    (63, N'Bình Thạnh'),
+    (63, N'Gò Vấp'),
+    (63, N'Phú Nhuận'),
+    (63, N'Tân Bình'),
+    (63, N'Tân Phú'),
+    (63, N'Thủ Đức'),
+    (63, N'Bình Chánh'),
+    (63, N'Nhà Bè'),
+    (63, N'Cần Giờ');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
